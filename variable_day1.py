1e2aa427fde60961e671eed67b3f826a2774a214
AleyJan <aliriayat507@gmail.com>
2026-05-07 15:57:50 +0500

    day1 variable

---
+++ variable_day1.py
@@ -0,0 +1,44 @@
+#Instructions
+#Create a new float, half, with the value 0.5.
+#Create a new string, intro, with the value "Hello! How are you?".
+#Create a new boolean, is_good, with the value True.
+
+
+# Create a variable half
+half = 0.5
+
+# Create a variable intro
+intro = "Hello! How are you?"
+
+# Create a variable is_good
+is_good = True
+
+
+
+#Add savings and new_savings and assign it to total_savings.
+#Use type() to print the resulting type of total_savings.
+
+
+savings = 100
+new_savings = 40
+
+# Calculate total_savings using savings and new_savings
+total_savings = savings + new_savings
+print(total_savings)
+
+# Print the type of total_savings
+print(type(total_savings))
+
+"""
+Calculate the sum of intro and intro and assign the result to doubleintro.
+Print out doubleintro. Did you expect this?
+
+"""
+
+intro = "Hello! How are you?"
+
+# Assign sum of intro and intro to doubleintro
+doubleintro = intro + intro
+
+# Print out doubleintro
+print(doubleintro)
